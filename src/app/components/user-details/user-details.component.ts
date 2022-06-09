78a37a50a0c4aacf9283a859f72d5aa2662a20fd
Max <tykhonenko.work@gmail.com>
2022-06-10 00:15:41 +0300

    sep dec 2021 angular 4

--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {UserService} from "../../services/user.service";
+import {IUsers} from "../../interfaces/IUsers";
 
 @Component({
   selector: 'app-user-details',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDetailsComponent implements OnInit {
 
-  constructor() { }
+  userDetailsObj:IUsers;
+
+  constructor(private activatedRoute:ActivatedRoute,
+              private userService:UserService) {
+
+  }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(({id})=>{
+      this.userService.getUser(id).subscribe(value => this.userDetailsObj=value)
+
+    });
   }
 
 }
